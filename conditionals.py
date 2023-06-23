6ca1b65cf5f4cc6f6ce1d89468ab1216f7b480fd
flopezt1508 <137199385+flopezt1508@users.noreply.github.com>
2023-06-23 12:26:54 -0400

    Add files via upload
    Ejemplos en python de condicionales

---
+++ conditionals.py
@@ -0,0 +1,44 @@
+
+x = 30
+
+if x < 30:
+    print("x is less than 30")
+else:
+    if x > 30:
+        print("x is greater than 30")
+    else:
+        print("x is 30")
+
+    
+    color ="blue"
+
+    if color == "red":
+        print("the color is red")
+    elif color == "yellow":
+        print("the color is yellow")
+    else:
+        print("the color is " + color )
+
+    name = "Francisco"
+    lastname = "López"
+
+    if name == "Francisco":
+        if lastname == "López":
+            print("You are " + name + " " + lastname)
+        else:
+            print("You are not " + name + " " + lastname)
+    else:
+        print("You are not " + name)
+
+x = 9
+if x > 2 and x < 10:
+    print("x es mayor que 2 y menor o igual que 10")
+if x > 2 or x < 20:
+    print("x es mayor que 2 y menor o igual que 20")
+
+z = 1
+y = 2
+if (not(z == y)):
+    print("z no es igual a y")
+
+
